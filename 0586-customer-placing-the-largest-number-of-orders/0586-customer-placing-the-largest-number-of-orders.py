04ebd8f5f268f06d7a93cf9e6730092750a4aee0
Mariia Timofeeva <146798113+mariia080693@users.noreply.github.com>
2025-06-11 21:46:04 +1000

    Time: 259 ms (94.76%), Space: 66.7 MB (91.83%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.py
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
 def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
+    return orders['customer_number'].mode().to_frame()
+
+    """
+    # long solution
+    
     orders_new = orders.groupby('customer_number')['order_number'].size().reset_index()
     max_count = orders_new['order_number'].max()
     orders_new = orders_new[orders_new['order_number'] == max_count]
     return orders_new[['customer_number']]
+    """
     
